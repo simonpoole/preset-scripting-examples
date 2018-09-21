0a6ffca6323cf2606c2ad56e2dace7c2cd0bb173
simonpoole <simon@poole.ch>
2018-09-21 09:15:33 +0200

    Add safe version of setting disused and removing tags

--- example_code.js
+++ example_code.js
@@ -122,15 +122,48 @@ function isObject(key) {
   }
   return false;
 }
-
+var presetItem;
 for (var key in Iterator(new java.util.HashSet(tags.keySet()))) {
+  
   if (isObject(key)) {
     tags.put('disused:'+key,tags.get(key));
+    presetItem = tags2PresetItem.get(key);
   }
   // it would be more elegant if we could use preset matching to determine
   // which tags should be removed
-  if (!key.startsWith('building') && !key.startsWith('roof')) {
+  if (presetItem !== null && presetItem.equals(tags2PresetItem.get(key)) {
     tags.remove(key);
+    java.lang.System.out.println('deleting ' + key);
+  }
+}
+tags.remove('set_to_disused_remove_tags');
+
+// example
+// set object to lifecycle disused state and remove any other keys except those that start with "building"
+//
+// currently the Preset objects are not accessible to the script so object keys need to be listed here
+// safe version of the above that will only remove keys if they matched the same preset
+var objects = ['shop','amenity','leisure','military'];
+
+function isObject(key) {
+  var objectsLength = objects.length;
+  for (var i = 0; i < objectsLength; i++) {
+    if (objects[i].equals(key)) {
+            return true;
+    }
+  }
+  return false;
+}
+var presetItem = null;
+for (var key in Iterator(new java.util.HashSet(tags.keySet()))) {
+  if (isObject(key)) {
+    tags.put('disused:'+key,tags.get(key));
+    presetItem = key2PresetItem.get(key);
+  }
+  if (typeof versionCode !== 'undefined' && versionCode > 703) {
+    if (presetItem !== null && presetItem.equals(key2PresetItem.get(key))) {
+      tags.remove(key);
+    }
   }
 }
 tags.remove('set_to_disused_remove_tags');
